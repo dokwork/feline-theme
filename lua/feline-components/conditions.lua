e27976bb7b5a58c30ada1f7587b4bcb55ed46552
Vladimir Popov <vladimir@dokwork.ru>
2022-04-30 17:12:59 +0300

    refactor: add functions sequentially instead of describe them in the constructor.

--- lua/feline-components/conditions.lua
+++ lua/feline-components/conditions.lua
@@ -7,28 +7,33 @@ vim.cmd(
 )
 vim.cmd('augroup end')
 
-local M = {
+local M = {}
 
-    ---@type fun(): boolean
-    ---@return boolean is_buffer_empty `true` when buffer has a single line with an empty string.
-    is_buffer_empty = function()
-        return vim.fn.line('$') == 1 and u.is_empty(vim.fn.getline(1))
-    end,
+---@type fun(): boolean
+---@return boolean is_buffer_empty `true` when buffer has a single line with an empty string.
+M.is_buffer_empty = function()
+    return vim.fn.line('$') == 1 and u.is_empty(vim.fn.getline(1))
+end
 
-    ---@type fun(): boolean
-    -- Uses 'tpope/vim-fugitive' to resolve is the file of the current buffer is in a git workspace.
-    ---@return boolean is_git_workspace `true` when the file of the current buffer is in git workspace.
-    ---@see vim.fn.FugitiveGitDir
-    is_git_workspace = function()
-        return not u.is_empty(vim.fn.FugitiveGitDir())
-    end,
+---@type fun(): boolean
+---@return boolean is_buffer_not_empty `true` when buffer has at least one symbol.
+M.is_buffer_not_empty = function()
+    return not M.is_buffer_empty()
+end
 
-    ---@type fun(): boolean
-    -- Checks the status of files whithin the current git workspace using `git status --poreclain`.
-    ---@return boolean is_git_changed `true` if one of the files in the current git workspace was changed.
-    is_git_changed = function()
-        return (vim.b.git_status and next(vim.b.git_status)) ~= nil
-    end,
-}
+---@type fun(): boolean
+-- Uses 'tpope/vim-fugitive' to resolve is the file of the current buffer is in a git workspace.
+---@return boolean is_git_workspace `true` when the file of the current buffer is in git workspace.
+---@see vim.fn.FugitiveGitDir
+M.is_git_workspace = function()
+    return not u.is_empty(vim.fn.FugitiveGitDir())
+end
+
+---@type fun(): boolean
+-- Checks the status of files whithin the current git workspace using `git status --poreclain`.
+---@return boolean is_git_changed `true` if one of the files in the current git workspace was changed.
+M.is_git_changed = function()
+    return (vim.b.git_status and next(vim.b.git_status)) ~= nil
+end
 
 return M
