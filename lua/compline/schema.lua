0422352d1ca813f43f551ecab1603b38635db4a9
Vladimir Popov <vladimir@dokwork.ru>
2022-06-18 12:21:15 +0300

    fix: tests have been passed

--- lua/compline/schema.lua
+++ lua/compline/schema.lua
@@ -73,13 +73,15 @@ end
 
 local PathToError = {}
 
-function PathToError:new()
-    local x = {}
+function PathToError:new(object, schema)
+    local p = {}
     -- pointer to the current validated position in the object
-    x.object_head = self.object_head
+    p.current_object = self.current_object
+    p.current_object_key = self.current_object_key
     -- pointer to the current validated position in the schema
-    x.schema_head = self.schema_head
-    setmetatable(x, {
+    p.current_schema = self.current_schema
+    p.current_schema_key = self.current_schema_key
+    setmetatable(p, {
         __index = self,
         __tostring = function(t)
             return string.format(
@@ -90,204 +92,53 @@ function PathToError:new()
             )
         end,
     })
-    return x
-end
-
-local function validate_const(value, schema, path)
-    local path = path:new()
-    path:add(value, schema)
-    if value ~= schema then
-        return false, path:wrong_value(schema, value)
-    end
-    return true
-end
-
-function PathToError:new_list(schema)
-    local path = self:new()
-    path:add({}, { list = schema })
-    return path
-end
-
-local function validate_list(list, el_type, path)
-    local path, err = path:new_list(el_type)
-    if err then
-        return false, err
+    if not (object or schema) then
+        return p
     end
 
-    for _, el in ipairs(list) do
-        _, err = M.validate(el, el_type, path)
-        if err then
-            return false, err
+    -- adding a new elements to the path --
+
+    if not p.current_schema then
+        p.object = object
+        p.schema = schema
+        p.current_object = object
+        p.current_schema = schema
+    elseif p.current_schema.list then
+        p.current_object[p.current_object_key] = object
+        p.current_object = object
+        p.current_object_key = nil
+    elseif p.current_schema.table then
+        local kv = p.current_schema.table[p.current_schema_key]
+        if not kv or kv.value then
+            -- key and value are already set, we should prepare a new key-value schema now
+            kv = {}
+            p.current_schema.table[p.current_schema_key] = kv
         end
-    end
-    return true
-end
-
-function PathToError:new_oneof(value, options)
-    local path = self:new()
-    path:add(value, { oneof = options })
-    if type(options) ~= 'table' then
-        return nil, path:wrong_schema_of('oneof', options)
-    end
-    return path
-end
-
-local function validate_oneof(value, options, path)
-    local path, err = path:new_oneof(value, options)
-    if err then
-        return false, err
-    end
-
-    for _, opt in ipairs(options) do
-        -- we do not pass any path here to avoid adding not applicable opt
-        if M.validate(value, opt) then
-            return true
-        end
-    end
-    return false, path:wrong_oneof(value, options)
-end
-
----@param schema table|table[] or list of key-value schemas, or key-value schema of all table
-function PathToError:new_table(schema)
-    local path = self:new()
-    path:add({}, { table = {} })
-    if type(schema) ~= 'table' then
-        return nil, path:wrong_schema_of('table', schema)
-    end
-    return path
-end
-
----@param schema table `{ key = type, value = type }`
-function PathToError:new_schema_kv(schema)
-    local path = self:new()
-    if not (schema.key and schema.value) then
-        table.insert(path.schema_head.table, schema)
-        return nil, path:wrong_kv_types_schema(schema)
-    end
-
-    table.insert(path.schema_head.table, {})
-
-    return path
-end
-
-function PathToError:add(value, schema)
-    if not self.schema_head then
-        self.object = value
-        self.schema = schema
-        self.object_head = value
-        self.schema_head = schema
-    elseif self.schema_head.table then
-        local kv = self.schema_head.table[#self.schema_head.table]
-        -- should be initialized a key of the table
         if not kv.key then
-            self.object_head[value] = '?'
+            -- we should set a key
             kv.key = schema
-            -- should be added a value for the key
+            p.current_object[object] = '?'
+            p.current_schema = schema
+            p.current_object = object
+            p.current_schema_key = nil
+            p.current_object_key = nil
         else
-            local key = next(self.object_head)
-            self.object_head[key] = value
+            -- a key is already set, we should set a value now
             kv.value = schema
-
-            self.object_head = value
-            self.schema_head = schema
+            p.current_object[p.current_object_key] = object
+            p.current_schema = schema
+            p.current_object = object
+            p.current_schema_key = nil
+            p.current_object_key = nil
         end
-    elseif self.schema_head.list then
-        table.insert(self.object_head, value)
-    elseif self.schema_head.oneof then
-        self.object_head = value
     else
-        self.object_head = value
-        self.schema_head = schema
-    end
-end
-
-local function validate_table(orig_tbl, kvs_schema, path)
-    local path, err = path:new_table(kvs_schema)
-    if err then
-        return false, err
+        p.current_object = object
+        p.current_schema = schema
+        p.current_object_key = nil
+        p.current_schema_key = nil
     end
 
-    local function split_list(list)
-        local required = {}
-        local optional = {}
-        for _, v in ipairs(list) do
-            if type(v) == 'table' and v.required then
-                table.insert(required, v)
-            else
-                table.insert(optional, v)
-            end
-        end
-        return required, optional
-    end
-
-    local function validate_single_option(unvalidated_tbl, kv_types, is_strict)
-        local path, err = path:new_schema_kv(kv_types)
-        if err then
-            return false, err
-        end
-
-        local at_least_one_passed = false
-
-        for k, v in pairs(unvalidated_tbl) do
-            _, err = M.validate(k, kv_types.key, path)
-            if not err then
-                _, err = M.validate(v, kv_types.value, path)
-                -- validation must be failed regadles of is_strict
-                -- if key is valid, but value is not
-                if err then
-                    return false, err
-                end
-
-                at_least_one_passed = true
-
-                -- remove validated key
-                unvalidated_tbl[k] = nil
-
-                -- constant can be checked only once
-                if M.is_const(kv_types.key) then
-                    return true
-                end
-            end
-        end
-
-        if is_strict and not at_least_one_passed then
-            return false, path:required_key_not_found(kv_types, unvalidated_tbl)
-        end
-
-        return true
-    end
-
-    local function validate_required_keys(unvalidated_tbl, kv_schemas)
-        for _, kv_schema in ipairs(kv_schemas) do
-            local _, err = validate_single_option(unvalidated_tbl, kv_schema, true)
-            if err then
-                return false, err
-            end
-        end
-        return true
-    end
-
-    local function validate_optional_keys(unvalidated_tbl, kv_schemas)
-        for _, kv_schema in ipairs(kv_schemas) do
-            local _, err = validate_single_option(unvalidated_tbl, kv_schema)
-            if err then
-                return false, err
-            end
-        end
-        return true
-    end
-
-    local unvalidated_tbl = vim.tbl_extend('error', {}, orig_tbl) -- this instance will be changed on validation
-    if kvs_schema.key and kvs_schema.value then
-        return validate_single_option(unvalidated_tbl, kvs_schema, true)
-    else
-        local required, optional = split_list(kvs_schema)
-        local _, err = validate_required_keys(unvalidated_tbl, required)
-        if err then
-            return false, err
-        end
-        return validate_optional_keys(unvalidated_tbl, optional)
-    end
+    return p
 end
 
 function PathToError:wrong_type(expected_type, obj)
@@ -344,6 +195,130 @@ function PathToError:required_key_not_found(kv_types, orig_table)
     return self
 end
 
+local function validate_const(value, schema, path)
+    local path = path:new(value, schema)
+    if value ~= schema then
+        return false, path:wrong_value(schema, value)
+    end
+    return path
+end
+
+local function validate_list(list, el_type, path)
+    local path = path:new({}, { list = el_type })
+    if type(list) ~= 'table' then
+        return false, path:wrong_type('table', list)
+    end
+
+    for i, el in ipairs(list) do
+        path.current_object_key = i
+        local _, err = M.validate(el, el_type, path)
+        if err then
+            return false, err
+        end
+    end
+    return true
+end
+
+local function validate_oneof(value, options, path)
+    local path = path:new(value, { oneof = options })
+    if type(options) ~= 'table' then
+        return nil, path:wrong_schema_of('oneof', options)
+    end
+    for _, opt in ipairs(options) do
+        -- we do not pass any path here to avoid adding not applicable opt
+        if M.validate(value, opt) then
+            return true
+        end
+    end
+    return false, path:wrong_oneof(value, options)
+end
+
+local function validate_table(orig_tbl, kvs_schema, path)
+    local path = path:new({}, { table = {} })
+    if type(kvs_schema) ~= 'table' then
+        return false, path:wrong_schema_of('table', kvs_schema)
+    end
+
+    if type(orig_tbl) ~= 'table' then
+        return false, path:wrong_type('table', orig_tbl)
+    end
+
+    local function split_list(list)
+        local required = {}
+        local optional = {}
+        for _, v in ipairs(list) do
+            if type(v) == 'table' and v.required then
+                table.insert(required, v)
+            else
+                table.insert(optional, v)
+            end
+        end
+        return required, optional
+    end
+
+    local function validate_key_value(unvalidated_tbl, kv_types, is_strict)
+        if not (kv_types.key and kv_types.value) then
+            return false, path:wrong_kv_types_schema(kv_types)
+        end
+
+        local at_least_one_passed = false
+
+        for k, v in pairs(unvalidated_tbl) do
+            path.current_object_key = k
+            local _, err = M.validate(k, kv_types.key, path)
+            if not err then
+                _, err = M.validate(v, kv_types.value, path)
+                -- if key is valid, but value is not,
+                -- then validation must be failed regadles of is_strict
+                if err then
+                    return false, err
+                end
+
+                at_least_one_passed = true
+
+                -- remove validated key
+                unvalidated_tbl[k] = nil
+
+                -- constant can be checked only once
+                if M.is_const(kv_types.key) then
+                    return true
+                end
+            end
+        end
+
+        if is_strict and not at_least_one_passed then
+            return false, path:required_key_not_found(kv_types, unvalidated_tbl)
+        end
+
+        return true
+    end
+
+    local function validate_keys(unvalidated_tbl, kv_schemas, is_strict)
+        for i, kv_schema in ipairs(kv_schemas) do
+            path.current_schema_key = i
+            local _, err = validate_key_value(unvalidated_tbl, kv_schema, is_strict)
+            if err then
+                return false, err
+            end
+        end
+        return true
+    end
+
+    -- this instance will be changed on validation
+    local unvalidated_tbl = vim.tbl_extend('error', {}, orig_tbl)
+    if kvs_schema.key and kvs_schema.value then
+        path.current_schema_key = 1
+        return validate_key_value(unvalidated_tbl, kvs_schema, true)
+    else
+        local required, optional = split_list(kvs_schema)
+        local _, err = validate_keys(unvalidated_tbl, required, true)
+        if err then
+            return false, err
+        end
+        return validate_keys(unvalidated_tbl, optional, false)
+    end
+end
+
 M.is_const = function(object)
     if type(object) == 'string' and not vim.tbl_contains(M.primirives(), object) then
         return true
@@ -386,10 +361,11 @@ M.validate = function(object, schema, path)
         return validate_const(object, type_value, path)
     end
     if type_name == 'any' then
+        path:new(object, schema)
         return true
     end
     -- flat constants or primitives
-    path:add(object, schema)
+    path = path:new(object, schema)
     local ok = type(object) == type_name or object == type_name
     if not ok then
         return false, path:wrong_type(type_name, object)
@@ -428,8 +404,8 @@ local theme_separators = {
     key = 'separators',
     value = {
         table = {
-            { key = 'left', value = M.separator },
-            { key = 'right', value = M.separator },
+            { key = 'left', value = M.theme_separator },
+            { key = 'right', value = M.theme_separator },
         },
     },
 }
@@ -439,7 +415,7 @@ M.theme_zone = {
         theme_separators,
         {
             key = 'string',
-            value = { list = M.highlight },
+            value = M.highlight,
         },
     },
 }
@@ -504,7 +480,7 @@ M.statusline = {
             required = true,
         },
         {
-            key = ' components',
+            key = 'components',
             value = { table = { key = 'string', value = M.component } },
             required = true,
         },
